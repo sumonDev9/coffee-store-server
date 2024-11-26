3e0977c2a878a57d18b2d08e48d557cac7d591bb
sumonDev9 <sumonmit678@gmail.com>
2024-11-26 16:29:04 +0530

    Added app.get route to fetch data from MongoDB server

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qqfyy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+// console.log(uri)
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -29,18 +29,23 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-
-
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
-     // send data to the  server  and mongodb
+    // step-2 all data get ui 
+    app.get('/coffee', async(req, res) => {
+      const cursor = coffeeCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    })
+
+
+     //step-1 send data to the  server  and mongodb
     app.post('/coffee', async(req, res) => {
         const newCoffee = req.body;
         console.log(newCoffee);
         const result = await coffeeCollection.insertOne(newCoffee);
         res.send(result);
-
-    })
+      })
     
 
     // Send a ping to confirm a successful connection
